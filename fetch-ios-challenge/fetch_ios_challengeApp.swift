1d9813a48dd04feb40f851e5159f2d2f14483491
Varun Samuel <varun531@gmail.com>
2024-09-11 23:23:53 -0700

    create basic app

---
+++ fetch-ios-challenge/fetch_ios_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  fetch_ios_challengeApp.swift
+//  fetch-ios-challenge
+//
+//  Created by Varun Samuel on 9/11/24.
+//
+
+import SwiftUI
+
+@main
+struct fetch_ios_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
